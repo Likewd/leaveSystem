3c5682d549fa7e9bb0350d8c868e39cd94883faf
likewd <seeheremeu@gmal.com>
2024-09-15 01:33:41 +0500

    db connection

---
+++ src/db/index.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+
+const connnectDB = async () => {
+    try {
+        const connectioninstance = await mongoose.connect(`${process.env.MONGEDB_URI}/${DB_NAME}`)
+
+        console.log(`\n MongoDB connected !! DB HOst ${connectioninstance.connection.host} `)
+
+
+    } catch (error) {
+        console.log("MONGO Connection ERROR", error)
+        process.exit(1)
+    }
+}
+
+
+export default connnectDB;

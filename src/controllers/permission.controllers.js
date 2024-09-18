87cfdfd84b4c0cd52904eaa379e8197f5320ad9a
likewd <seeheremeu@gmal.com>
2024-09-19 01:54:37 +0500

    working on permission model

--- src/controllers/permission.controllers.js
+++ src/controllers/permission.controllers.js
@@ -8,13 +8,19 @@ const createPermission = asyncHandler(async (req, res) => {
     console.log(name);
 
     if (!name) {
-        throw new ApiError(400, "Permission Name required")
+        throw new ApiError(
+            "Permission Name required",
+            400,
+            )
     }
 
     const existedPermission = await Permission.findOne({ name })
     if (existedPermission) {
 
-        throw new ApiError(409, "permission already existed")
+        throw new ApiError(
+            "permission already existed",
+            409, 
+        )
 
     }
     const permission = await Permission.create({ name, description })
@@ -22,10 +28,57 @@ const createPermission = asyncHandler(async (req, res) => {
 
 
     return res.status(201).json(
-        new ApiResponse(200, permission, "permission registered Successfully")
+        new ApiResponse("permission registered Successfully", 201,
+            permission,
+        )
+
     )
 })
 
+const getAllPermission = asyncHandler(async (req, res) => {
+    const allPermission = await Permission.find({})
+    if (allPermission.length === 0) {
+        return res.status(200).json(new ApiResponse("No permissions found", 200, []));
+    }
+    return res.status(200).json(
+        new ApiResponse("Permissions fetched successfully", 200,
+            allPermission,
+        )
+
+    )
+
+})
+
+const deletePermission = asyncHandler(async (req, res) => {
+    const { _id } = req.params
+    if (!_id) {
+        throw new ApiError(
+           "Permission ID is required",
+            400,
+        )
+    }
+
+    const deletePermission = await
+        Permission.findByIdAndDelete(_id)
+
+    if (!deletePermission) {
+        throw new ApiError(
+            "Permission Not Found",
+            404,
+        )
+
+    }
+    return res.status(200).json(
+        new ApiResponse("permission Delete Successfully", 200,
+            deletePermission,
+        )
+
+    )
+
+})
 export {
-    createPermission
+    createPermission,
+    deletePermission,
+    getAllPermission
+
 }
--- src/routes/permission.route.js
+++ src/routes/permission.route.js
@@ -1,8 +1,15 @@
 import { Router } from "express";
-import { createPermission } from "../controllers/permission.controllers.js";
+import {
+    createPermission,
+    deletePermission,
+    getAllPermission
+} from "../controllers/permission.controllers.js";
 
 const router = Router()
 
-router.route("/create").post(createPermission)
+router.route("/create")
+    .post(createPermission)
+router.route("/delete/:_id").delete(deletePermission)
+router.route("/all").get(getAllPermission)
 
 export default router
